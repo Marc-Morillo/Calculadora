0dd42f7709c216529d510785083617fe45ccba6f
Marc-Morillo <86896794+Marc-Morillo@users.noreply.github.com>
2022-02-03 20:43:01 -0400

    Pasando programa a github

---
+++ Calculadora_de_Prestamos/Calculadora_de_Prestamos/FrmConvertidor.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Calculadora_de_Prestamos
+{
+    partial class FrmConvertidor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMontodelPrestamo = new System.Windows.Forms.Label();
+            this.lblCuotasMensuales = new System.Windows.Forms.Label();
+            this.lblInteresMensual = new System.Windows.Forms.Label();
+            this.TxtPrestamo = new System.Windows.Forms.TextBox();
+            this.lblTipodePrestamo = new System.Windows.Forms.Label();
+            this.CbxTipoPrestamo = new System.Windows.Forms.ComboBox();
+            this.TxtTasa = new System.Windows.Forms.TextBox();
+            this.CbxCantidadCuotas = new System.Windows.Forms.ComboBox();
+            this.TxtResultadoCuota = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.lblCuotaResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMontodelPrestamo
+            // 
+            this.lblMontodelPrestamo.AutoSize = true;
+            this.lblMontodelPrestamo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMontodelPrestamo.Location = new System.Drawing.Point(140, 66);
+            this.lblMontodelPrestamo.Name = "lblMontodelPrestamo";
+            this.lblMontodelPrestamo.Size = new System.Drawing.Size(320, 45);
+            this.lblMontodelPrestamo.TabIndex = 0;
+            this.lblMontodelPrestamo.Text = "Monto del préstamo:";
+            // 
+            // lblCuotasMensuales
+            // 
+            this.lblCuotasMensuales.AutoSize = true;
+            this.lblCuotasMensuales.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCuotasMensuales.Location = new System.Drawing.Point(12, 246);
+            this.lblCuotasMensuales.Name = "lblCuotasMensuales";
+            this.lblCuotasMensuales.Size = new System.Drawing.Size(460, 45);
+            this.lblCuotasMensuales.TabIndex = 0;
+            this.lblCuotasMensuales.Text = "Cantidad de cuotas mensuales:";
+            this.lblCuotasMensuales.Click += new System.EventHandler(this.lblCuotasMensuales_Click);
+            // 
+            // lblInteresMensual
+            // 
+            this.lblInteresMensual.AutoSize = true;
+            this.lblInteresMensual.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblInteresMensual.Location = new System.Drawing.Point(41, 187);
+            this.lblInteresMensual.Name = "lblInteresMensual";
+            this.lblInteresMensual.Size = new System.Drawing.Size(419, 45);
+            this.lblInteresMensual.TabIndex = 0;
+            this.lblInteresMensual.Text = "Tasa de intéres mensual (%):";
+            this.lblInteresMensual.Click += new System.EventHandler(this.lblInteresMensual_Click);
+            // 
+            // TxtPrestamo
+            // 
+            this.TxtPrestamo.Location = new System.Drawing.Point(466, 72);
+            this.TxtPrestamo.Name = "TxtPrestamo";
+            this.TxtPrestamo.Size = new System.Drawing.Size(457, 39);
+            this.TxtPrestamo.TabIndex = 1;
+            // 
+            // lblTipodePrestamo
+            // 
+            this.lblTipodePrestamo.AutoSize = true;
+            this.lblTipodePrestamo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTipodePrestamo.Location = new System.Drawing.Point(181, 127);
+            this.lblTipodePrestamo.Name = "lblTipodePrestamo";
+            this.lblTipodePrestamo.Size = new System.Drawing.Size(279, 45);
+            this.lblTipodePrestamo.TabIndex = 0;
+            this.lblTipodePrestamo.Text = "Tipo de prestamo:";
+            // 
+            // CbxTipoPrestamo
+            // 
+            this.CbxTipoPrestamo.FormattingEnabled = true;
+            this.CbxTipoPrestamo.Location = new System.Drawing.Point(466, 132);
+            this.CbxTipoPrestamo.Name = "CbxTipoPrestamo";
+            this.CbxTipoPrestamo.Size = new System.Drawing.Size(457, 40);
+            this.CbxTipoPrestamo.TabIndex = 3;
+            this.CbxTipoPrestamo.SelectedIndexChanged += new System.EventHandler(this.CbxTipoPrestamo_SelectedIndexChanged);
+            // 
+            // TxtTasa
+            // 
+            this.TxtTasa.Location = new System.Drawing.Point(466, 193);
+            this.TxtTasa.Name = "TxtTasa";
+            this.TxtTasa.ReadOnly = true;
+            this.TxtTasa.Size = new System.Drawing.Size(457, 39);
+            this.TxtTasa.TabIndex = 4;
+            this.TxtTasa.TextChanged += new System.EventHandler(this.TxtTasa_TextChanged);
+            // 
+            // CbxCantidadCuotas
+            // 
+            this.CbxCantidadCuotas.FormattingEnabled = true;
+            this.CbxCantidadCuotas.Location = new System.Drawing.Point(466, 251);
+            this.CbxCantidadCuotas.Name = "CbxCantidadCuotas";
+            this.CbxCantidadCuotas.Size = new System.Drawing.Size(457, 40);
+            this.CbxCantidadCuotas.TabIndex = 3;
+            this.CbxCantidadCuotas.SelectedIndexChanged += new System.EventHandler(this.CbxCantidadCuotas_SelectedIndexChanged);
+            // 
+            // TxtResultadoCuota
+            // 
+            this.TxtResultadoCuota.Location = new System.Drawing.Point(466, 311);
+            this.TxtResultadoCuota.Name = "TxtResultadoCuota";
+            this.TxtResultadoCuota.ReadOnly = true;
+            this.TxtResultadoCuota.Size = new System.Drawing.Size(457, 39);
+            this.TxtResultadoCuota.TabIndex = 5;
+            this.TxtResultadoCuota.TextChanged += new System.EventHandler(this.TxtResultadoCuota_TextChanged);
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BtnCalcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnCalcular.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnCalcular.Location = new System.Drawing.Point(466, 389);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(457, 114);
+            this.BtnCalcular.TabIndex = 6;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = false;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // lblCuotaResultado
+            // 
+            this.lblCuotaResultado.AutoSize = true;
+            this.lblCuotaResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCuotaResultado.Location = new System.Drawing.Point(183, 305);
+            this.lblCuotaResultado.Name = "lblCuotaResultado";
+            this.lblCuotaResultado.Size = new System.Drawing.Size(277, 45);
+            this.lblCuotaResultado.TabIndex = 0;
+            this.lblCuotaResultado.Text = "Cuotas mensuales";
+            this.lblCuotaResultado.Click += new System.EventHandler(this.lblCuotasMensuales_Click);
+            // 
+            // FrmConvertidor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1013, 582);
+            this.Controls.Add(this.lblCuotaResultado);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.TxtResultadoCuota);
+            this.Controls.Add(this.CbxCantidadCuotas);
+            this.Controls.Add(this.TxtTasa);
+            this.Controls.Add(this.CbxTipoPrestamo);
+            this.Controls.Add(this.lblTipodePrestamo);
+            this.Controls.Add(this.TxtPrestamo);
+            this.Controls.Add(this.lblInteresMensual);
+            this.Controls.Add(this.lblCuotasMensuales);
+            this.Controls.Add(this.lblMontodelPrestamo);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmConvertidor";
+            this.Text = " Calculadora de Prestamos Personales";
+            this.Load += new System.EventHandler(this.FrmConvertidor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMontodelPrestamo;
+        private System.Windows.Forms.Label lblCuotasMensuales;
+        private System.Windows.Forms.Label lblInteresMensual;
+        private System.Windows.Forms.TextBox TxtPrestamo;
+        private System.Windows.Forms.Label lblTipodePrestamo;
+        private System.Windows.Forms.ComboBox CbxTipoPrestamo;
+        private System.Windows.Forms.TextBox TxtTasa;
+        private System.Windows.Forms.ComboBox CbxCantidadCuotas;
+        private System.Windows.Forms.TextBox TxtResultadoCuota;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Label lblCuotaResultado;
+    }
+}
+
---
+++ Calculadora_de_Prestamos/Calculadora_de_Prestamos/FrmConvertidor.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Calculadora_de_Prestamos.CustomControlItem;
+using Calculadora_de_Prestamos.CustomControlItem2;
+using BussinessLayer;
+
+namespace Calculadora_de_Prestamos
+{
+    public partial class FrmConvertidor : Form
+    {
+        public bool SeCalculo { get; set; } = false;
+        public FrmConvertidor()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmConvertidor_Load(object sender, EventArgs e)
+        {
+            LoadCombobox();
+            LoadCombobox2();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            CalcularPrestamo();
+        }
+
+        private void CbxTipoPrestamo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            VerInteres();
+            if (SeCalculo)
+            {
+                CalcularPrestamo();
+            }
+        }
+
+        private void CbxCantidadCuotas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SeCalculo)
+            {
+                CalcularPrestamo();
+            }
+        }
+
+        private void CalcularPrestamo()
+        {
+            ComboboxItem SeleccionadoTipodePrestamo = CbxTipoPrestamo.SelectedItem as ComboboxItem;
+            ComboboxItem2 SeleccionadoMes = CbxCantidadCuotas.SelectedItem as ComboboxItem2;
+
+            try
+            {
+                if (string.IsNullOrEmpty(TxtPrestamo.Text))
+                {
+                    MessageBox.Show("Debe ingresar un monto del prestamo", "Advertencia");
+
+                }
+                else if (SeleccionadoTipodePrestamo.Value == null)
+                {
+                    MessageBox.Show("Debe seleccionar un tipo de prestamo", "Advertencia");
+                }
+                else if (SeleccionadoMes.Value == null)
+                {
+                    MessageBox.Show("Debe seleccionar la cantidad de meses", "Advertencia");
+                }
+                else
+                {
+                    Calculador calculador = new Calculador();
+                    int TipoMes = (int)SeleccionadoMes.Value;
+                    int TipoPrestamo = (int)SeleccionadoTipodePrestamo.Value;
+                    double Prestamo = Convert.ToDouble(TxtPrestamo.Text);
+
+                    double resultado = Math.Round(calculador.Calcular(Prestamo, TipoMes, TipoPrestamo), 2);
+                    TxtResultadoCuota.Text = resultado.ToString();
+
+                    SeCalculo = true;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Su monto debe de ingresar solo cantidades numericas", "Adventerncia");
+            }
+        }
+
+        private void VerInteres()
+        {
+            ComboboxItem SeleccionadoTipodePrestamo = CbxTipoPrestamo.SelectedItem as ComboboxItem;
+       
+            if(SeleccionadoTipodePrestamo.Text == "Personal")
+            {
+                TxtTasa.Text = "22%";
+            }
+            else if (SeleccionadoTipodePrestamo.Text == "Automóvil")
+            {
+                TxtTasa.Text = "12%";
+            }
+            else if (SeleccionadoTipodePrestamo.Text == "Hipotecario")
+            {
+                TxtTasa.Text = "8%";
+            }
+        }
+        private void LoadCombobox()
+        {
+            ComboboxItem Opcionpordefecto = new ComboboxItem();
+            Opcionpordefecto.Text = "Por favor seleccione una opcion";
+            Opcionpordefecto.Value = null;
+
+            ComboboxItem OpcionPersonal = new ComboboxItem();
+            OpcionPersonal.Text = "Personal";
+            OpcionPersonal.Value = 1;
+
+            ComboboxItem OpcionAutomovil = new ComboboxItem();
+            OpcionAutomovil.Text = "Automóvil";
+            OpcionAutomovil.Value = 2;
+
+            ComboboxItem OpcionHipotecario = new ComboboxItem();
+            OpcionHipotecario.Text = "Hipotecario";
+            OpcionHipotecario.Value = 3;
+
+            CbxTipoPrestamo.Items.Add(Opcionpordefecto);
+            CbxTipoPrestamo.Items.Add(OpcionPersonal);
+            CbxTipoPrestamo.Items.Add(OpcionAutomovil);
+            CbxTipoPrestamo.Items.Add(OpcionHipotecario);
+            CbxTipoPrestamo.SelectedItem = Opcionpordefecto;
+        }
+
+        private void LoadCombobox2()
+        {
+            ComboboxItem2 OpcionDefecto = new ComboboxItem2();
+            OpcionDefecto.Text = "Por favor seleccione una opcion";
+            OpcionDefecto.Value = null;
+
+            ComboboxItem2 Mes1 = new ComboboxItem2();
+            Mes1.Text = "12";
+            Mes1.Value = 1;
+
+            ComboboxItem2 Mes2 = new ComboboxItem2();
+            Mes2.Text = "18";
+            Mes2.Value = 2;
+
+            ComboboxItem2 Mes3 = new ComboboxItem2();
+            Mes3.Text = "24";
+            Mes3.Value = 3;
+
+            ComboboxItem2 Mes4 = new ComboboxItem2();
+            Mes4.Text = "30";
+            Mes4.Value = 4;
+
+            ComboboxItem2 Mes5 = new ComboboxItem2();
+            Mes5.Text = "36";
+            Mes5.Value = 5;
+
+            ComboboxItem2 Mes6 = new ComboboxItem2();
+            Mes6.Text = "42";
+            Mes6.Value = 6;
+
+            ComboboxItem2 Mes7 = new ComboboxItem2();
+            Mes7.Text = "48";
+            Mes7.Value = 7;
+
+            ComboboxItem2 Mes8 = new ComboboxItem2();
+            Mes8.Text = "54";
+            Mes8.Value = 8;
+
+            ComboboxItem2 Mes9 = new ComboboxItem2();
+            Mes9.Text = "60";
+            Mes9.Value = 9;
+
+            ComboboxItem2 Mes10 = new ComboboxItem2();
+            Mes10.Text = "66";
+            Mes10.Value = 10;
+
+            ComboboxItem2 Mes11 = new ComboboxItem2();
+            Mes11.Text = "72";
+            Mes11.Value = 11;
+
+            ComboboxItem2 Mes12 = new ComboboxItem2();
+            Mes12.Text = "78";
+            Mes12.Value = 12;
+
+            ComboboxItem2 Mes13 = new ComboboxItem2();
+            Mes13.Text = "84";
+            Mes13.Value = 13;
+
+            ComboboxItem2 Mes14 = new ComboboxItem2();
+            Mes14.Text = "90";
+            Mes14.Value = 14;
+
+            ComboboxItem2 Mes15 = new ComboboxItem2();
+            Mes15.Text = "96";
+            Mes15.Value = 15;
+
+            ComboboxItem2 Mes16 = new ComboboxItem2();
+            Mes16.Text = "102";
+            Mes16.Value = 16;
+
+            ComboboxItem2 Mes17 = new ComboboxItem2();
+            Mes17.Text = "108";
+            Mes17.Value = 17;
+
+            ComboboxItem2 Mes18 = new ComboboxItem2();
+            Mes18.Text = "114";
+            Mes18.Value = 18;
+
+            ComboboxItem2 Mes19 = new ComboboxItem2();
+            Mes19.Text = "120";
+            Mes19.Value = 19;
+
+            CbxCantidadCuotas.Items.Add(OpcionDefecto);
+            CbxCantidadCuotas.Items.Add(Mes1);
+            CbxCantidadCuotas.Items.Add(Mes2);
+            CbxCantidadCuotas.Items.Add(Mes3);
+            CbxCantidadCuotas.Items.Add(Mes4);
+            CbxCantidadCuotas.Items.Add(Mes5);
+            CbxCantidadCuotas.Items.Add(Mes6);
+            CbxCantidadCuotas.Items.Add(Mes7);
+            CbxCantidadCuotas.Items.Add(Mes8);
+            CbxCantidadCuotas.Items.Add(Mes9);
+            CbxCantidadCuotas.Items.Add(Mes10);
+            CbxCantidadCuotas.Items.Add(Mes11);
+            CbxCantidadCuotas.Items.Add(Mes12);
+            CbxCantidadCuotas.Items.Add(Mes13);
+            CbxCantidadCuotas.Items.Add(Mes14);
+            CbxCantidadCuotas.Items.Add(Mes15);
+            CbxCantidadCuotas.Items.Add(Mes16);
+            CbxCantidadCuotas.Items.Add(Mes17);
+            CbxCantidadCuotas.Items.Add(Mes18);
+            CbxCantidadCuotas.Items.Add(Mes19);
+            CbxCantidadCuotas.SelectedItem = OpcionDefecto;
+
+        }
+        private void lblCuotasMensuales_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblInteresMensual_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtResultadoCuota_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtTasa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
